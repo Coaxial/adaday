17d458968535710fccf53e87dfce56d0b6685435
Coaxial <py@poujade.org>
2016-04-21 09:41:18 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+gulp.task('test', function() {
+  console.log('No test runner configured.');
+  process.exit(1);
+});
