146cbb405cf27e1db0cb6f053c0c6d2b6eca8b43
Pierre-Yves Poujade <py@poujade.org>
2016-04-22 14:19:15 -0400

    Lint code

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,20 @@
 
 const gulp = require('gulp');
 const mocha = require('gulp-spawn-mocha');
+const eslint = require('gulp-eslint');
 
 gulp.task('default', () => {
   return gulp.start('test');
 });
 
-gulp.task('test', () => {
+gulp.task('test', ['lint'], () => {
   return gulp.src(['test/test.js'], {read: false})
     .pipe(mocha());
+});
+
+gulp.task('lint', () => {
+  return gulp.src(['**/*.js', '!node_modules/**'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
