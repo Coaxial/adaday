ee4f6cb0e30194a5750e3e7d36e53add3434981c
Pierre-Yves Poujade <py@poujade.org>
2016-04-22 16:19:21 -0400

    Implement the CulturePub module

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@
 const gulp = require('gulp');
 const mocha = require('gulp-spawn-mocha');
 const eslint = require('gulp-eslint');
+const runSequence = require('run-sequence');
+
+const test_files = 'test/**/*_test.js';
+const lib_files = 'lib/**/*.js';
+const app_file = 'index.js';
 
 gulp.task('default', () => {
-  return gulp.start('test');
-});
-
-gulp.task('test', ['lint'], () => {
-  return gulp.src(['test/test.js'], {read: false})
-    .pipe(mocha());
+  return gulp.start('lint_and_test');
 });
 
 gulp.task('lint', () => {
@@ -18,4 +18,25 @@ gulp.task('lint', () => {
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
+});
+
+gulp.task('test', () => {
+  process.env.NOCK_BACK_MODE = 'lockdown';
+  return gulp.src([test_files], { read: false })
+    .pipe(mocha());
+});
+
+// Use this when developing
+gulp.task('dev', () => {
+  const all_js_files = [lib_files, test_files, app_file];
+  gulp.watch(all_js_files, ['lint_and_test'], { read: false });
+});
+
+// Composite tasks
+gulp.task('lint_and_test', (done) => {
+  return runSequence(
+    'lint',
+    'test',
+    done
+  );
 });
