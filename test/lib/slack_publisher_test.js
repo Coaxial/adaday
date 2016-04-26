738bd26b7db5972dd768f6d7bbebf22c010bf66b
Coaxial <py@poujade.org>
2016-04-25 21:49:29 -0400

    Implement Slack publisher

---
+++ test/lib/slack_publisher_test.js
@@ -0,0 +1,95 @@
+/* eslint-env mocha */
+'use strict';
+
+const testHelper = require('../test_helper').create();
+const chai = require('chai');
+const assert = chai.assert;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const proxyquire = require('proxyquire');
+const nock = require('nock');
+
+const subject = proxyquire(
+  '../../lib/slack_publisher.js', {
+    './config_reader': testHelper.mocks.configReader
+  }
+).create();
+
+describe('slackPublisher', () => {
+  describe('#publishAd', () => {
+    let test_ad;
+
+    beforeEach(function assignTestAd() {
+      test_ad = {
+        video_url: 'https://example.com/video.mp4',
+        advertiser_name: 'Test brand',
+        ad_title: 'Test ad',
+        ad_country: 'Testlandia',
+        ad_year: '1998',
+        ad_director: 'Test Mctesty',
+        ad_agency: 'Test agency'
+      };
+    });
+
+    afterEach(function cleanNock() {
+      nock.cleanAll();
+    });
+
+    context('with an unsuccessful request', () => {
+      beforeEach(function mockApi() {
+        const api_host = 'https://hooks.slack.com';
+        // example endpoint format:
+        // /services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX
+        // cf https://api.slack.com/incoming-webhooks
+        const endpoint_regex = /services\/T[a-z0-9]{8}\/B[a-z0-9]{8}\/[a-z0-9]{24}/i;
+        const matchAnyRequestBody = () => true;
+        const HTTP_INTERNAL_ERROR = 500;
+
+        nock(api_host)
+          .post(endpoint_regex, matchAnyRequestBody)
+          .reply(HTTP_INTERNAL_ERROR, 'Internal server error');
+      });
+
+      it('throws with a diagnostic message', () => {
+        return assert.isRejected(subject.publishAd(test_ad), /hooks\.slack\.com.*500.*Internal server error/);
+      });
+    });
+
+    context('with a successful request', () => {
+      beforeEach(function mockApi() {
+        const api_host = 'https://hooks.slack.com';
+        // example endpoint format:
+        // /services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX
+        // cf https://api.slack.com/incoming-webhooks
+        const endpoint_regex = /services\/T[a-z0-9]{8}\/B[a-z0-9]{8}\/[a-z0-9]{24}/i;
+        const matchAnyRequestBody = () => true;
+        const HTTP_OK = 200;
+
+        nock(api_host)
+          .post(endpoint_regex, matchAnyRequestBody)
+          .reply(HTTP_OK, 'ok');
+      });
+
+      it('returns the Slack webhook payload', () => {
+        const expected_payload = {
+          channel: '#test',
+          username: 'AdADay',
+          text: "Today's random ad is now available, watch it while it's hot!\n_Bot by @pierre, source code: <https://github.com/coaxial/adaday>._",
+          icon_emoji: ':tv:',
+          attachments: [
+            {
+              title: 'Test brand: Test ad',
+              title_link: 'https://example.com/video.mp4',
+              text: 'Originally aired in 1998, in Testlandia. Directed by Test Mctesty, created by Test agency.'
+            }
+          ]
+        };
+
+        return subject.publishAd(test_ad)
+        .then((actual_payload) => {
+          return assert.deepEqual(actual_payload, expected_payload);
+        })
+      });
+    });
+  });
+});
